3675267de430a01c19e9ebb1734f76654570eee6
surya <suryaherdiyanto@gmail.com>
2017-12-10 13:52:25 +0800

    update

--- config/api.php
+++ config/api.php
@@ -70,7 +70,7 @@ return [
     |
     */
 
-    'domain' => env('API_DOMAIN', 'http://libraryapi-app.herokuapp.com'),
+    'domain' => env('API_DOMAIN', 'https://libraryapi-app.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
