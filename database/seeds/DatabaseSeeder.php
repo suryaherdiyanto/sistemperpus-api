d47a0792b16ff79b23eade320c22a8956f1b458d
surya <suryaherdiyanto@gmail.com>
2017-12-10 14:31:51 +0800

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
         App\User::create([
           'name' => 'surya',
           'email' => 'surya@gmail.com',
-          'password' => app('hash')->make('123qwe'),
+          'password' => '123qwe',
           'role_id' => 3
         ]);
     }
